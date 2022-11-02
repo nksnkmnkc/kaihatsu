bf1d280aa4950a71bac22d9a623cf48e594c0fd3
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-11-02 05:40:25 +0000

    [Add]いいね機能/コメント機能の部分テンプレート

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,22 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  # フォローをした、されたの関係
+  # フォローをした、されたの関係(子要素)
   #フォローする側のUserから見て、フォローされる側のUserを(Relationshipを経由して)集める。なのでfollower_id(フォローする側)
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   #フォローされる側のUserから見て、フォローしてくる側のUserを(Relationship経由で)集める。なのでfollowed_id(フォローされる側)
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  # 一覧画面で使う
-  # Relationship（passive_relationshipsを経由して「follower」モデルのUser(フォローする側)を集めることを「followers」と定義
+  # 一覧画面で使う（孫要素）
+  # Relationship（passive_relationshipsを経由して「follower」モデルのUser(フォローする側)を集めることを「followers」と定義。自分をフォローしてくれている相手を取得する。相手がフォロワー。フォーリンキーはfollowed.孫要素→follower
   has_many :followers, through: :passive_relationships, source: :follower
-  # Relationship（active_relationshipsを経由して「followed」モデルのUser(フォローされた側)を集めることを「followings」と定義 自分がフォローしている相手を取得する。自分がフォロワー。フォーリンキーはfollower。孫要素はどこから取得するのか→followed
-
+  # Relationship（active_relationshipsを経由して「followed」モデルのUser(フォローされた側)を集めることを「followings」と定義。 自分がフォローしている相手を取得する。自分がフォロワー。フォーリンキーはfollower。孫要素はどこから取得するのか→followed
   has_many :followings, through: :active_relationships, source: :followed
+
+  has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50 }
