269a10ece07b31dd38f80b4ac460778c5d1a4e4f
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-11-09 06:39:51 +0000

    [Finish]7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,16 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  # 記録アプリ機能
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } #今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } #先週
+  
+  #scope :スコープの名前, -> { 条件式 } 
+  #Time.zone.now.all_dayで１日を表す 1day.ago.all_dayで昨日を表す
+
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
