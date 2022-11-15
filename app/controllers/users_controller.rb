a7a946c666a3b227f7e22ec0af36f7b00e4dfe71
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-11-15 11:10:40 +0000

    [Finish]ゲストログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
  before_action :authenticate_user!
+ before_action :ensure_guest_user, only: [:edit]
 
   def show
     @user = User.find(params[:id])
@@ -43,8 +44,12 @@ class UsersController < ApplicationController
 
   def ensure_correct_user
     @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
     unless @user == current_user
       redirect_to user_path(current_user)
     end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
+#ゲストログイン機能
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
